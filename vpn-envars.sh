24a6a44d0a8a6feb225813cd21ae52ae2d1342de
Fernando <fernandohds564@gmail.com>
2020-03-23 10:08:08 -0300

    ENH: Add configdb and constants servers IP redefinition to envars file.

--- vpn-envars.sh
+++ vpn-envars.sh
@@ -1,1 +1,9 @@
+
+# Variables redefinition to work via VPN:
+# NOTE: This method only works for interactive terminals
+# In order to have the same result in non-interactive terminals
+# this code should be placed at the beggining of the ~/.bashrc file
+# right after the import of bashrc-sirius file.
 export EPICS_CA_ADDR_LIST="10.0.38.46:60000 10.0.38.59:60000"
+export SIRIUS_URL_CONSTS=http://10.0.38.42/control-system-constants
+export SIRIUS_URL_CONFIGDB=http://10.0.38.42/config-db
